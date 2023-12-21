4444806243afff78f89ebf2e96a8878865ce6fe6
minju <loverminju88@naver.com>
2023-12-21 11:23:50 +0900

    231221 - @RequestParam, @ModelAttribute 검색 설정

--- BoardWeb231218/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb231218/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,8 +1,13 @@
 package com.springbook.view.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springbook.biz.board.BoardVO;
@@ -11,9 +16,25 @@ import com.springbook.biz.board.impl.BoardServiceImpl;
 @Controller
 public class BoardController {
 	
+	// @ModelAttribute 메소드의 실행 결과로 리턴된 객체는 자동으로 Model에 저장
+	@ModelAttribute("map") // @RequestMapping 적용 메소드 보다 몬저 호출
+	public Map<String, String> searchMap() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("제목", "title");
+		map.put("내용", "content");
+		return map;
+	}
+	
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(BoardServiceImpl service, Model model) {
+	public String getBoardList(
+			@RequestParam(value="ch1", defaultValue="title", required=false) String ch1, 
+			@RequestParam(value="ch2", defaultValue="", required=false) String ch2, 
+			BoardServiceImpl service, Model model) {
+		// value: 화면으로부터 전달될 파라미터 이름
+		// defaultValue: 화면으로부터 전달될 파라미터 정보가 없을 때, 설정할 기본값 - null에 대한 걱정 X
+		// required: 파라미터의 생략 여부 
 		System.out.println("글 목록 검색 처리");
+		System.out.println("ch1(검색 조건): " + ch1 + ", ch2(검색 단어): " + ch2);
 		
 //		HttpSession session = request.getSession();
 //		session.setAttribute("li", li);
--- BoardWeb231218/src/main/webapp/getBoardList.jsp
+++ BoardWeb231218/src/main/webapp/getBoardList.jsp
@@ -20,10 +20,25 @@
 <body>
 	<div align=center>
 		<h1>목록보기</h1>
-		<h3>${ userName }님 환영합니다!..<a href="logout.do">Log-out</a>
-<!-- 	jsp 였다면 session.getAttribute 로 받아와야 하지만, el 표기법이라 바로 가져올 수 있음 -->
+		<h3>${ userName }님
+			환영합니다!..<a href="logout.do">Log-out</a>
+			<!-- 	jsp 였다면 session.getAttribute 로 받아와야 하지만, el 표기법이라 바로 가져올 수 있음 -->
 		</h3>
 		<hr>
+		<form action="getBoardList.do" method="post">
+			<table border=1 cellpadding="0" cellspacing="0">
+				<tr>
+					<td align=right><select name="ch1">
+<!-- 						<option value="title">제목</option> -->
+<!-- 						<option value="content">내용</option> -->
+							<c:forEach items="${ map }" var="option">
+								<option value="${ option.value }">${ option.key }</option>
+							</c:forEach>
+					</select> <input name="ch2" type="text" /> <input type=submit value="검색" />
+					</td>
+				</tr>
+			</table>
+		</form>
 		<table border=1>
 			<tr>
 				<td>번호</td>
@@ -32,9 +47,6 @@
 				<td>등록일</td>
 				<td>조회수</td>
 			</tr>
-			<%--  		<%  --%>
-			<!--  		for (BoardVO m : li) { -->
-			<%--  		%>  --%>
 			<c:forEach items="${ li }" var="m">
 
 				<tr>
@@ -45,9 +57,6 @@
 					<td><a href="deleteBoard.do?seq=${ m.getSeq() }">${ m.getCnt() }</a></td>
 				</tr>
 			</c:forEach>
-			<%--  		<%  --%>
-			<!-- 		} -->
-			<%-- 		%>  --%>
 		</table>
 		<a href="write.jsp">새글등록</a>
 		<hr>
