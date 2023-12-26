63485c2a86417bd34c3b93856f9f78f145a2292e
minju <loverminju88@naver.com>
2023-12-26 11:39:43 +0900

    231226 - SqlSessionTemplate로 Dao 구현

--- SpringProject231222/src/main/java/com/springbook/biz/board/BoardDaoMybatis.java
+++ SpringProject231222/src/main/java/com/springbook/biz/board/BoardDaoMybatis.java
@@ -2,19 +2,16 @@ package com.springbook.biz.board;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BoardDaoMybatis extends SqlSessionDaoSupport implements BoardDao {
+public class BoardDaoMybatis implements BoardDao {
 	
 	@Autowired
-	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
-		super.setSqlSessionFactory(sqlSessionFactory);
-	}
-
+	public SqlSessionTemplate mybatis;
+	
 	@Override
 	public List<BoardVO> getBoardList(String ch1, String ch2) {
 		System.out.println("===> Mybatis getBoardList");
@@ -24,11 +21,11 @@ public class BoardDaoMybatis extends SqlSessionDaoSupport implements BoardDao {
 		vo.setCh2(ch2);
 		
 		if (ch2.equals("")) {
-			return getSqlSession().selectList("BoardDao.select_sql");
+			return mybatis.selectList("BoardDao.select_sql");
 		} else if (ch1.equals("writer")) {
-			return getSqlSession().selectList("BoardDao.select_writer_sql", vo);
+			return mybatis.selectList("BoardDao.select_writer_sql", vo);
 		} else if (ch1.equals("title")) {
-			return getSqlSession().selectList("BoardDao.select_title_sql", vo);
+			return mybatis.selectList("BoardDao.select_title_sql", vo);
 		}
 		return null;
 	}
@@ -36,31 +33,31 @@ public class BoardDaoMybatis extends SqlSessionDaoSupport implements BoardDao {
 	@Override
 	public BoardVO getBoard(BoardVO vo) {
 		System.out.println("===> Mybatis getBoard");
-		return (BoardVO) getSqlSession().selectOne("BoardDao.select1_sql", vo);
+		return (BoardVO) mybatis.selectOne("BoardDao.select1_sql", vo);
 	}
 
 	@Override
 	public void insert(BoardVO vo) {
 		System.out.println("===> Mybatis insert");
-		getSqlSession().insert("BoardDao.insert_sql", vo);
+		mybatis.insert("BoardDao.insert_sql", vo);
 	}
 
 	@Override
 	public void delete(BoardVO vo) {
 		System.out.println("===> Mybatis delete");
-		getSqlSession().delete("BoardDao.delete_sql", vo);
+		mybatis.delete("BoardDao.delete_sql", vo);
 	}
 
 	@Override
 	public void cntUpdate(BoardVO vo) {
 		System.out.println("===> Mybatis cntUpdate");
-		getSqlSession().update("BoardDao.cnt_sql", vo);
+		mybatis.update("BoardDao.cnt_sql", vo);
 	}
 
 	@Override
 	public void update(BoardVO vo) {
 		System.out.println("===> Mybatis update");
-		getSqlSession().update("BoardDao.update_sql", vo);
+		mybatis.update("BoardDao.update_sql", vo);
 	}
 	
 }
